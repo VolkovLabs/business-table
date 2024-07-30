f90608f55a84d8ca23121c9e8b581070e800819c
Mikhail Volkov <mikhail@volkovlabs.io>
2024-07-30 09:02:01 -0400

    Update tests

---
+++ test/panel.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@grafana/plugin-e2e';
+
+test.describe('Volkovlabs Table Panel', () => {
+  test('Check grafana version', async ({ grafanaVersion }) => {
+    console.log('Grafana version: ', grafanaVersion);
+    expect(grafanaVersion).toEqual(grafanaVersion);
+  });
+
+  test('Should display a Table', async ({ gotoDashboardPage, page, gotoPanelEditPage }) => {
+    /**
+     * Go To Panels dashboard panels.json
+     * return dashboardPage
+     */
+    await gotoDashboardPage({ uid: 'O4tc_E6Gz' });
+
+    await expect(page.getByRole('heading', { name: 'Table' }).first()).toBeVisible();
+  });
+});
