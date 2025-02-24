40b41666b863d8afc1be034a2601d7fdaa43c9e6
vitPinchuk <vit.pinchuck@gmail.com>
2025-02-24 13:21:33 +0300

    remove package

--- src/utils/group.ts
+++ src/utils/group.ts
@@ -1,7 +1,29 @@
 import { DataFrame, Field } from '@grafana/data';
-import { findField } from '@volkovlabs/grafana-utils';
 
 import { FieldSource } from '@/types';
+
+/**
+ * Find field
+ */
+export const findField = <TValue = unknown>(
+  series: DataFrame[],
+  predicateFn: (field: Field, frame: DataFrame) => boolean
+): Field<TValue> | undefined => {
+  for (let i = 0; i < series.length; i += 1) {
+    const frame = series[i];
+
+    const field = frame.fields.find((field) => predicateFn(field, frame));
+
+    /**
+     * Field found
+     */
+    if (field) {
+      return field;
+    }
+  }
+
+  return undefined;
+};
 
 /**
  * Get Field Key
