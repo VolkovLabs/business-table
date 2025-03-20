03979c9fbed441048dc97dd437390f19b8a3827d
Vitali Pinchuk <vit.pinchuck@gmail.com>
2025-03-20 11:00:02 +0300

    upd CI test

--- src/hooks/useUserStorage.test.ts
+++ src/hooks/useUserStorage.test.ts
@@ -32,15 +32,28 @@ describe('useUserStorage', () => {
     );
   });
 
-  it('Should call setItem and store data correctly', async () => {
+  it('Should return undefined when userStorage is null', async () => {
+    jest.mocked(usePluginUserStorage).mockReturnValue(null);
     const { result } = renderHook(() => useUserStorage(version));
-    const testKey = 'testKey';
-    const testData = { foo: 'bar' };
 
+    await expect(result.current.getItem('testKey')).resolves.toBeUndefined();
+  });
+
+  it('Should store item correctly', async () => {
+    const { result } = renderHook(() => useUserStorage(version));
     await act(async () => {
-      await result.current.setItem(testKey, testData);
+      await result.current.setItem('testKey', { data: 'value' });
     });
 
-    expect(mockStorage.setItem).toHaveBeenCalledWith(testKey, JSON.stringify(testData));
+    expect(mockStorage.setItem).toHaveBeenCalledWith('testKey', JSON.stringify({ data: 'value' }));
+  });
+
+  it('Should return parsed data from storage', async () => {
+    mockStorage.getItem.mockResolvedValueOnce(JSON.stringify({ data: 'value' }));
+    const { result } = renderHook(() => useUserStorage(version));
+    await act(async () => {
+      const data = await result.current.getItem('testKey');
+      expect(data).toEqual({ data: 'value' });
+    });
   });
 });
