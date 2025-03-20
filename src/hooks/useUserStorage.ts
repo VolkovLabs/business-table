a45c0b307db42aa5362c27e81572e7d4ee9eb002
Vitali Pinchuk <vit.pinchuck@gmail.com>
2025-03-20 10:48:48 +0300

    update useUserStorage

--- src/hooks/useUserStorage.ts
+++ src/hooks/useUserStorage.ts
@@ -1,4 +1,7 @@
 import { usePluginUserStorage } from '@grafana/runtime';
+import { useCallback } from 'react';
+
+import { PluginUserStorage } from '@/types';
 
 import { useLocalStorage } from './useLocalStorage';
 
@@ -8,29 +11,55 @@ import { useLocalStorage } from './useLocalStorage';
  */
 export const useUserStorage = (version: number) => {
   const localStorage = useLocalStorage(version);
-  /**
-   * Avoid plugin crush for grafana versions lt 11.5.0
-   */
-  try {
-    const storage = usePluginUserStorage();
-    const update = async <T>(key: string, data: T) => {
-      await storage.setItem(key, JSON.stringify(data));
-      return data;
-    };
 
-    const get = async (key: string) => {
-      const storageValue = await storage.getItem(key);
+  /**
+   * Define user storage
+   */
+  let userStorage: PluginUserStorage | null = null;
+
+  /**
+   * Define get user storage
+   * should be useCallback
+   * to avoid using userStorage.getItem directly
+   * using userStorage.getItem directly in useEffect can cause an infinite call
+   */
+  const getUserStorageValue = useCallback(
+    async (key: string) => {
+      if (!userStorage) {
+        return undefined;
+      }
+      const storageValue = await userStorage.getItem(key);
 
       if (storageValue) {
         return JSON.parse(storageValue);
       }
 
       return undefined;
+    },
+    [userStorage]
+  );
+
+  /**
+   * Avoid plugin crush for grafana versions lt 11.5.0
+   */
+  try {
+    /**
+     * use usePluginUserStorage hook
+     */
+    const storage = usePluginUserStorage();
+    userStorage = storage;
+
+    /**
+     * override storage.setItem to be like useLocalStorage
+     */
+    const update = async <T>(key: string, data: T) => {
+      await storage.setItem(key, JSON.stringify(data));
+      return data;
     };
 
     return {
       setItem: update,
-      getItem: get,
+      getItem: getUserStorageValue,
     };
   } catch {
     return localStorage;
